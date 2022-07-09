f6b5e6856361da12d7c172dd5e1f9117dcd30557
Markiyan <melnik.markiyan@gmail.com>
2022-07-09 19:58:46 +0300

    Added form menu

--- javascript.js
+++ javascript.js
@@ -2,6 +2,8 @@ const DEFAULT_SIZE = 16;
 let size = DEFAULT_SIZE;
 let mode = 'default';
 const container = document.querySelector('.container');
+const slider = document.getElementById('range');
+const output = document.getElementById('display');
 
 
 // Makes a playground (grid) size x size
@@ -16,9 +18,17 @@ function makeRows(size) {
   
 makeRows(size);
 
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
+
 // Changes the background color for grid-items
 container.addEventListener('mouseover', (e) => {
     if (e.target.classList.contains('grid-item')) {
-        e.target.style.backgroundColor = 'black';
+        if (mode === 'default') {
+            e.target.style.backgroundColor = 'black';
+        } else if (mode === 'eraser') {
+            e.target.style.backgroundColor = 'white'
+        }
     }
 });
